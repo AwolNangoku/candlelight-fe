a7b30e265a5e0c49eaf839f01906703567e79e46
ncedile nkonyana <ncedile.nkonyana@sovtech.com>
2023-06-02 14:47:40 +0200

    feat: adds initial work on onboarding flow

--- src/components/layout/PageLayout/index.tsx
+++ src/components/layout/PageLayout/index.tsx
@@ -1,14 +1,28 @@
 import { Stack } from "@chakra-ui/react";
 import React from "react";
+import NavigationBar from "./navigation-bar";
 
 interface IPageLayoutProps {
+  navItems?: {
+    icon: React.ReactNode
+    onIconClick: () => void
+  }[]
   children: React.ReactNode
 }
 
-const PageLayout = ({ children }: IPageLayoutProps) => {
+const PageLayout = ({
+  navItems,
+  children
+}: IPageLayoutProps) => {
   return (
-    <Stack>
-      {children}
+    <Stack
+      w='100%'
+      flexDirection='column'
+    >
+      <NavigationBar navItems={navItems} />
+      <Stack w='100%'>
+        {children}
+      </Stack>
     </Stack>
   )
 }
---
+++ src/pages/onboarding/index.tsx
@@ -0,0 +1,72 @@
+import React from "react"
+import { PageLayout } from "../../components/layout"
+import { Bell, Gear, MagnifyingGlass } from "phosphor-react"
+import { Input, InputGroup, InputLeftElement, Stack, Text } from "@chakra-ui/react"
+
+const Onboarding = () => {
+  const navitems: {
+    icon: React.ReactNode
+    onIconClick: () => void
+  }[] = [{
+    icon: <MagnifyingGlass  size={24}/>,
+    onIconClick: () => console.log('Visit')
+  },
+  {
+    icon: <Gear size={24}/>,
+    onIconClick: () => console.log('Settings')
+  },
+  {
+    icon: <Bell size={24}/>,
+    onIconClick: () => console.log('Notifications')
+  }
+]
+  return (
+    <PageLayout navItems={navitems}>
+      <Stack w='100%'>
+        <Stack w='100%' justifyContent='center' align='center'>
+          <Text
+            fontWeight={400}
+            fontSize='24px'
+            lineHeight='24px'
+          >
+            What are some of Your Favourite Cuisines & meals?
+          </Text>
+        </Stack>
+
+        <Stack w='100%' justifyContent='flex-start'>
+          <Text
+            fontWeight={400}
+            fontSize='14px'
+            lineHeight='14px'
+            color='#616161'
+          >
+            Select or Search some Cuisine Types
+          </Text>
+          <Stack w='100%'>
+            <InputGroup w='100%'>
+              <InputLeftElement pointerEvents='none'>
+                <MagnifyingGlass size={24} />
+              </InputLeftElement>
+              <Input type='tex' placeholder='Search' />
+            </InputGroup>
+          </Stack>
+        </Stack>
+
+        <Stack w='100%' justifyContent='flex-start'>
+          <Text
+            fontWeight={500}
+            fontSize='14px'
+            lineHeight='22px'
+            color='#272727'
+          >
+            Tap to add
+          </Text>
+          <Stack w='100%'>
+            
+          </Stack>
+        </Stack>
+      </Stack>
+    </PageLayout>
+  )
+}
+export default Onboarding
